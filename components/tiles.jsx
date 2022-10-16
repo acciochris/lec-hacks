2f8180a10f70f10a23b37a7c87815f1c54fae1ac
Kavitha Palanisamy <archita@kavithas-air.lan>
2022-10-16 12:53:48 -0700

    background color

--- components/tiles.jsx
+++ components/tiles.jsx
@@ -1,5 +1,20 @@
+import styles from "../styles/Tiles.module.css";
+
 const CategoryTile = () => {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <div className={styles.box}>
+        <h2>Machine Learning</h2>
+        <p>
+          Projects submitted to this track mus have at least one machine
+          learning or artificial intelligence component or application in the
+          final product. Be prepared to explain you r machine learning
+          components and how you implemented the to our judges in your demo
+          video
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default CategoryTile;
