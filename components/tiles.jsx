59c1299f9b9b9a98814746556c147b9395999658
Kavitha Palanisamy <archita@kavithas-air.lan>
2022-10-16 13:16:02 -0700

    stff

--- components/tiles.jsx
+++ components/tiles.jsx
@@ -3,15 +3,50 @@ import styles from "../styles/Tiles.module.css";
 const CategoryTile = () => {
   return (
     <div>
-      <div className={styles.box}>
-        <h2>Machine Learning</h2>
-        <p>
-          Projects submitted to this track mus have at least one machine
-          learning or artificial intelligence component or application in the
-          final product. Be prepared to explain you r machine learning
-          components and how you implemented the to our judges in your demo
-          video
-        </p>
+      <h1>hi</h1>
+      <div className={styles.container}>
+        <div className={styles.grid}>
+          <div className={styles.box}>
+            <h2>Machine Learning</h2>
+            <p>
+              Projects submitted to this track mus have at least one machine
+              learning or artificial intelligence component or application in
+              the final product. Be prepared to explain your machine learning
+              components and how you implemented the to our judges in your demo
+              video
+            </p>
+          </div>
+          <div className={styles.box}>
+            <h2>Machine Learning</h2>
+            <p>
+              Projects submitted to this track mus have at least one machine
+              learning or artificial intelligence component or application in
+              the final product. Be prepared to explain your machine learning
+              components and how you implemented the to our judges in your demo
+              video
+            </p>
+          </div>
+          <div className={styles.box}>
+            <h2>Machine Learning</h2>
+            <p>
+              Projects submitted to this track mus have at least one machine
+              learning or artificial intelligence component or application in
+              the final product. Be prepared to explain your machine learning
+              components and how you implemented the to our judges in your demo
+              video
+            </p>
+          </div>
+          <div className={styles.box}>
+            <h2>Machine Learning</h2>
+            <p>
+              Projects submitted to this track mus have at least one machine
+              learning or artificial intelligence component or application in
+              the final product. Be prepared to explain your machine learning
+              components and how you implemented the to our judges in your demo
+              video
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
