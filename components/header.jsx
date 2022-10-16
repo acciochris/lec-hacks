64b85be0629c96f3501c443ff3b294e995cab771
Kavitha Palanisamy <archita@kavithas-air.lan>
2022-10-16 14:36:16 -0700

    fixed ur problem

--- components/header.jsx
+++ components/header.jsx
@@ -4,7 +4,12 @@ import styles from "../styles/Header.module.css";
 const Header = () => {
   return (
     <div className={styles.HeaderContainer}>
-      <h1 className={styles.head}>LEC Hacks</h1>
+      <div className={styles.center}>
+        <h1 className={styles.head}>LEC Hacks</h1>
+        <a className={styles.subheading} href="google.com">
+          March 20-21, 2021
+        </a>
+      </div>
     </div>
   );
 };
