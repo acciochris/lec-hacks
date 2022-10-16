2498b6180a22959a8740398086b69bf6f4d3a1d3
Veerrohit Veeravadivel <veerrohitv08@gmail.com>
2022-10-16 12:36:17 -0700

    random

---
+++ components/tiles.jsx
@@ -0,0 +1,5 @@
+const CategoryTitle = () => {
+  return <div>Hi</div>;
+};
+
+export default CategoryTile;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import CategoryTile from "../components/tiles";
+import header from "../components/header";
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <header />
+      <CategoryTile />
+    </div>
   );
 }
