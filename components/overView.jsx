86c33217483e42c68402a7486a63c4cef3554263
Kavitha Palanisamy <archita@kavithas-air.lan>
2022-10-16 15:03:42 -0700

    finished overview

--- components/overView.jsx
+++ components/overView.jsx
@@ -3,7 +3,10 @@ import styles from "../styles/OverView.module.css";
 const overView = () => {
   return (
     <div className={styles.container}>
-      <h2>Event Overview</h2>
+      <div className={styles.center}>
+        <h2>Event Overview</h2>
+      </div>
+      <br />
       <p>
         <x>Mission Statement:</x> LEC Hacks is a virtual hackathon for
         high-school students hosted by Lynbrook Engineering Club of LHS. Through
@@ -11,10 +14,15 @@ const overView = () => {
         interest in the field of engineering and ecourage people to explore the
         various branches of engineering
       </p>
+      <br />
       <p>
         <x>Location:</x> Online via Discord(Invite Only)
       </p>
-      <p>Theme: Wellness</p>
+      <br />
+      <p>
+        <x>Theme:</x> Wellness
+      </p>
+      <br />
       <p>
         <x>Competitor Goal:</x> Complete a project in line with this years
         themem and following one of our 5 tracks. The main objective is to push
