19a0adc4ea112014041800dcb70d09df7df937e9
Veerrohit Veeravadivel <veerrohitv08@gmail.com>
2022-10-19 08:03:14 -0700

    fixed footer and aravinds really bad css skills

--- components/footer.jsx
+++ components/footer.jsx
@@ -2,28 +2,40 @@ import React from "react";
 import styles from "../styles/Socials.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram, faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "next/link";
 
 const footer = () => {
   return (
     <div className={styles.full}>
-      <div className={styles.footer}>
-        <div className={styles.text}>
-          <div className={styles.row}>
-            <div className={styles.row}>
-              <FontAwesomeIcon icon={faInstagram} className={styles.icon} />
-              <p className={styles.text}>@lynbrookengineering</p>
-            </div>
-            <div className={styles.rowIcon}>
-              <FontAwesomeIcon icon={faGoogle} className={styles.icon} />
-              <p className={styles.text}>lynbrookengineers@gmail.com</p>
-            </div>
+      <div className={styles.row}>
+        <div className={styles.oneOfOne}>
+          <h3 className={styles.title}>Socials</h3>
+        </div>
+      </div>
+      <div className={styles.row}>
+        <div className={styles.oneOfTwo}>
+          <FontAwesomeIcon icon={faInstagram} className={styles.icon} />
+          <div className={styles.text}>
+            <a href="https://www.instagram.com/" className={styles.link}>
+              @lynbrookengineering
+            </a>
           </div>
-          <div className={styles.center}>
-            <p className={styles.footerText}>
-              Webpage created by Aravindkrishna Ariuvdainmabi and Veerrohit
-              Veeravadivel
-            </p>
+        </div>
+        <div className={styles.oneOfTwo}>
+          <FontAwesomeIcon icon={faGoogle} className={styles.icon} />
+          <div className={styles.text}>
+            <a href="https://www.google.com/" className={styles.link}>
+              lynbrookengineers@gmail.com
+            </a>
           </div>
+        </div>
+      </div>
+      <div className={styles.row}>
+        <div className={styles.oneOfOne}>
+          <p className={styles.smallText}>
+            Webpage created by Veerrohit Veeravadivel and Aravindkrishna
+            Arivudanambi
+          </p>
         </div>
       </div>
     </div>
