6a3a29c600cf1ba13ab26a7a5c80e537f8965491
Chris Liu <acciochris@outlook.com>
2023-09-15 22:19:36 +0000

    Copy template from roomGPT

--- components/arrowDown.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "../styles/ArrowDown.module.css";
-
-const Arrow = ({ func }) => {
-  return <button className={styles.scrolldown} onClick={() => func}></button>;
-};
-
-export default Arrow;
