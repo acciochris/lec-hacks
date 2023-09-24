bf12797ce0b225668defb53d0d654edbfeaedc98
Chris Liu <59787860+acciochris@users.noreply.github.com>
2023-09-24 03:21:25 +0000

    Remove ogimage and twitter

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,30 +3,13 @@ import { Metadata } from "next";
 import "../styles/globals.css";
 
 let title = "LEC Hacks";
-let description = "Generate your dream room in seconds.";
-let ogimage = "https://roomgpt-demo.vercel.app/og-image.png";
-let sitename = "roomGPT.io";
+let description = "Lynbrook Engineering Club Hackathon";
 
 export const metadata: Metadata = {
   title,
   description,
   icons: {
     icon: "/favicon.png",
-  },
-  openGraph: {
-    images: [ogimage],
-    title,
-    description,
-    url: "https://roomgpt-demo.vercel.app",
-    siteName: sitename,
-    locale: "en_US",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    images: [ogimage],
-    title,
-    description,
   },
 };
 
